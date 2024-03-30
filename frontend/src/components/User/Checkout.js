8f6b097958409c25aa63385adab64c7483f237b6
NavneetSharma18 <mr.navneetgaur@gmail.com>
2024-03-30 15:49:22 +0530

    checkout

--- frontend/src/components/User/Checkout.js
+++ frontend/src/components/User/Checkout.js
@@ -1,17 +1,28 @@
-import React,{useState,useEffect} from 'react'
-import { useNavigate }  from "react-router-dom";
+import React,{useState,useEffect} from 'react';
+import {Link,json,useNavigate} from 'react-router-dom';
+import axios from 'axios';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useSelector, useDispatch } from 'react-redux'
-import axios from 'axios';
+import {checkLogout} from '../../redux/userApi/';
+import {incProductQty,decProductQty,removeProductCart} from '../../redux/productApi/';
 axios.defaults.withCredentials = true
+
 
 
 const Checkout = ()=>{
 
+const auth         = useSelector((state) => state.loginRes.isUserLogin);
+const userData     = useSelector((state) => state.loginRes.authData);
 
-  const auth  = useSelector((state) => state.loginRes.userId);
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
+const cartItems    = useSelector((state) => state.cartItem.cartItems);
+const total        = useSelector((state) => state.cartItem.totalPrice);
+const subTotal     = useSelector((state) => state.cartItem.subTotal);
+const shippingCost = useSelector((state) => state.cartItem.shippingCost);
+
+const navigate     = useNavigate();
+const dispatch     = useDispatch();
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
  
 
@@ -21,7 +32,7 @@ const Checkout = ()=>{
             
                   <div className="h-screen grid grid-cols-3">
                   <div className="lg:col-span-2 col-span-3 space-y-8 px-12">
-                      <div className="mt-8 p-4 relative flex flex-col sm:flex-row sm:items-center bg-white shadow rounded-md">
+                      <div className="mt-8 p-4  flex flex-col sm:flex-row sm:items-center bg-white shadow rounded-md">
                           <div className="flex flex-row items-center border-b sm:border-b-0 w-full sm:w-auto pb-4 sm:pb-0">
                               <div className="text-yellow-500">
                                   <svg xmlns="http://www.w3.org/2000/svg" className="w-6 sm:w-5 h-6 sm:h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -116,50 +127,42 @@ const Checkout = ()=>{
                   <div className="col-span-1 bg-white lg:block hidden">
                       <h1 className="py-6 border-b-2 text-xl text-gray-600 px-8">Order Summary</h1>
                       <ul className="py-6 border-b space-y-6 px-8">
-                          <li className="grid grid-cols-6 gap-2 border-b-1">
-                              <div className="col-span-1 self-center">
-                                  <img src="https://bit.ly/3oW8yej" alt="Product" className="rounded w-full" />
-                              </div>
-                              <div className="flex flex-col col-span-3 pt-2">
-                                  <span className="text-gray-600 text-md font-semi-bold">Studio 2 Headphone</span>
-                                  <span className="text-gray-400 text-sm inline-block pt-2">Red Headphone</span>
-                              </div>
-                              <div className="col-span-2 pt-3">
-                                  <div className="flex items-center space-x-2 text-sm justify-between">
-                                      <span className="text-gray-400">2 x €30.99</span>
-                                      <span className="text-pink-400 font-semibold inline-block">€61.98</span>
-                                  </div>
-                              </div>
-                          </li>
-                          <li className="grid grid-cols-6 gap-2 border-b-1">
-                              <div className="col-span-1 self-center">
-                                  <img src="https://bit.ly/3lCyoSx" alt="Product" className="rounded w-full" />
-                              </div>
-                              <div className="flex flex-col col-span-3 pt-2">
-                                  <span className="text-gray-600 text-md font-semi-bold">Apple iPhone 13</span>
-                                  <span className="text-gray-400 text-sm inline-block pt-2">Phone</span>
-                              </div>
-                              <div className="col-span-2 pt-3">
-                                  <div className="flex items-center space-x-2 text-sm justify-between">
-                                      <span className="text-gray-400">1 x €785</span>
-                                      <span className="text-pink-400 font-semibold inline-block">€785</span>
-                                  </div>
-                              </div>
-                          </li>
+                      {
+                            
+                            cartItems.length>0 ? cartItems.map((item,index)=>
+                            
+                                <li className="grid grid-cols-6 gap-2 border-b-1">
+                                    <div className="col-span-1 self-center">
+                                        <img src={API_BASE_URL+item.product_image} alt="Product" className="rounded w-full" />
+                                    </div>
+                                    <div className="flex flex-col col-span-3 pt-2">
+                                        <span className="text-gray-600 text-md font-semi-bold">{item.product_title}</span>
+                                       
+                                    </div>
+                                    <div className="col-span-2 pt-3">
+                                        <div className="flex items-center space-x-2 text-sm justify-between">
+                                            <span className="text-gray-400">{item.qty} x ₹{item.product_price}</span>
+                                            <span className="text-pink-400 font-semibold inline-block">₹{item.product_price}</span>
+                                        </div>
+                                    </div>
+                                </li>
+                         )
+                         : 'No item in cart'
+                       } 
                       </ul>
                       <div className="px-8 border-b">
                           <div className="flex justify-between py-4 text-gray-600">
                               <span>Subtotal</span>
-                              <span className="font-semibold text-pink-500">€846.98</span>
+                              <span className="font-semibold text-pink-500">₹{subTotal}</span>
                           </div>
                           <div className="flex justify-between py-4 text-gray-600">
                               <span>Shipping</span>
-                              <span className="font-semibold text-pink-500">Free</span>
+                              <span className="font-semibold text-pink-500">₹{shippingCost}</span>
                           </div>
                       </div>
                       <div className="font-semibold text-xl px-8 flex justify-between py-8 text-gray-600">
                           <span>Total</span>
-                          <span>€846.98</span>
+                          <span>₹{total}</span>
                       </div>
                   </div>
               </div>
